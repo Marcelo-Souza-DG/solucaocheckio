818c4a7de474312427fbdb4e866512240d0bb8c7
Marcelo-Souza-DG <mcspro.dg@gmail.com>
2022-03-28 17:06:09 -0300

    codando melhor

--- mult_two.py
+++ mult_two.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     print(mult_two(3, 2))
     
     # These "asserts" are used for self-checking and not for an auto-testing
+
     assert mult_two(3, 2) == 6
     assert mult_two(1, 0) == 0
-    print("Coding complete? Click 'Check' to earn cool rewards!")
